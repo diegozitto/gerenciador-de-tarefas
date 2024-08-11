2087a452c1ec1ffc53b5a83e7b64a2a2f1e77588
Diego Zito <diegomirandazito@gmail.com>
2024-08-10 23:07:46 -0300

    Adicionando segundo teste de api: teste de bem vindo

--- src/main/java/com/gerenciador_de_tarefas/controller/TesteApiController.java
+++ src/main/java/com/gerenciador_de_tarefas/controller/TesteApiController.java
@@ -1,19 +1,28 @@
 package com.gerenciador_de_tarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 /**
  Anotação "@RestController" para ser considerado api
- Anotação "@GetMapping" para poder definir o endereço do teste
+ Anotação "@GetMapping" para poder definir o endereço do teste e o tipo de verbo HTTP que será utilizado:
+ -GetMapping  = Get
+ -PostMapping = Post
+
+ Anotação (@RequestParam(name = "nome")String nome), ao usar essa anotação estamos dizendo que ao executar o teste de
+ api é necessário passar um parâmetro no Postman cuja a "key" precisa se chamar "nome" e nesse o caso o seu valor será
+ o nome do usuário testando, o dado é setado como sendo do tipo String.
   */
 
 @RestController
 public class TesteApiController {
 
     @GetMapping("/teste-api")
-    private String teste(){
-        return "Sucesso";
-    }
+    private String teste(){return "Sucesso";}
+
+    @GetMapping("/teste-api-bem-vindo")
+    private String testeBemVindo(@RequestParam(name = "nome") String nome){
+        return "Olá " + nome + ", seja muito bem vindo!";}
 }
